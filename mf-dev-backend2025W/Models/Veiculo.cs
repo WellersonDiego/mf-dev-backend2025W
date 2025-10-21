58d3a254ab9c70a3637c35c94a37cec73ecff231
Wellerson Diego <wellersondiego.wd@gmail.com>
2025-10-21 19:13:46 -0300

    Views, Incluir-Detalhes-Delete

--- mf-dev-backend2025W/Models/Veiculo.cs
+++ mf-dev-backend2025W/Models/Veiculo.cs
@@ -19,10 +19,12 @@ namespace mf_dev_backend2025W.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação")]
+        [Display(Name = "Ano de Fabricação")]
 
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo")]
+        [Display(Name = "Ano do Modelo")]
 
         public int AnoModelo { get; set; }
     }
